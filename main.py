bf99c014e43b07008b14091429698a86f97e98de
heber <0.apazaheber.1@gmail.com>
2022-11-11 15:04:26 -0400

    python first basic

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello, my first code line in Python")
+
+print("hola, mi primera linea de codigo en python")
+
+print(12 + 12)
+
+# This is a Commentary
+my_name = input("What is your name?")
+print("My name is:", my_name)
+
