77727e837acf03a7d7f895b6a745230fe2a3056c
heber <0.apazaheber.1@gmail.com>
2022-11-11 15:48:21 -0400

    commit of data type on python

---
+++ data-types.py
@@ -0,0 +1,21 @@
+#STRING 
+my_name = 'Heber'
+print('my name is', my_name)
+#function to see the data type
+print(type(my_name))
+
+#INTEGER
+my_age = 12
+print('my age is', my_age)
+print(type(my_age))
+
+#BOOLEAN
+is_single = False
+print('is single =', is_single)
+print(type(is_single))
+
+#INPUT
+my_height = input('what is your height: ')
+print('my height is',my_height)
+print(type(my_height))
+
