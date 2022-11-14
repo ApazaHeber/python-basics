dc2af24e19619572d3cea441934dc53e0394a195
heber <0.apazaheber.1@gmail.com>
2022-11-13 22:30:27 -0400

    my notebok of numbers

---
+++ numbers/numbers.py
@@ -0,0 +1,31 @@
+# to see what data type is we use the function 'type'
+
+
+lives = 3
+print(type(lives))
+
+temperature = 12.12
+print(type(temperature))
+
+lives = 12 + 12
+print(lives)
+
+# rest or sum number
+lives -= 1
+print(lives)
+
+lives += 5
+print(lives)
+
+#notation science
+num = 4500000000000000000.1
+print(num)
+
+num2 = 0.0000000000001
+print(num2)
+
+# my finances
+monday = 20
+tuesday = 34
+friday = 22
+print((monday + tuesday + friday)/3)
