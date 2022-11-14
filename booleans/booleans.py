9637a5282af58f5ce07ddd87cd33de1307044ac7
heber <0.apazaheber.1@gmail.com>
2022-11-14 12:16:21 -0400

    my branch with changes in booleans

---
+++ booleans/booleans.py
@@ -0,0 +1,15 @@
+#there are 2 states Flase & False
+# Always use 
+is_single = True
+print(type(is_single))
+
+is_single = False
+print(is_single)
+
+#transform to opposite
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)
+
