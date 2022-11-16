b5d0082f15017c13df91c9d4aa566fe6490c05fc
heber <0.apazaheber.1@gmail.com>
2022-11-16 11:42:51 -0400

    I added new information about operatos-conditional

---
+++ arithmetic-operators.py
@@ -0,0 +1,27 @@
+print(10 + 10)
+print(10 - 2)
+print(2 * 2)
+print(4 / 2)
+
+print('hola' + ' mundo')
+print('hola' * 3) # multiply x 3 the hola word
+
+#others
+# module operator (el residuo)
+print(10 % 3)
+# it give us the interger number (nos devuelve el numero entero)
+print(10 // 3)
+#exponential operators (exponencial)
+print(2 ** 3)
+#divide by zero does not exist
+#print(10 / 0) #is an error
+
+
+# Arithmetic order to resolve a problem
+#python respect Arithmetic operators
+# 1)parentesis
+# 2)exponente
+# 3)multiplicacion y division
+# 4)suma y resta
+# 5)izquierda a derecha
+
