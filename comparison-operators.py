1fb6f625d8ba1c135382f030bd31982fa65d7fdd
heber <0.apazaheber.1@gmail.com>
2022-11-16 12:00:39 -0400

    I added information about operators and conditional

---
+++ comparison-operators.py
@@ -0,0 +1,25 @@
+#Basic comparion operators
+# the result always is a bollean
+# < # mayor que
+# > # menor que
+print(7 > 3)
+
+# >= # mayor o igual que
+# <= # menor o igual que
+print(2 >= 1)
+
+# ==  igual a 
+print(6 == 7)
+
+# != # distinto de
+print(6 != 10)
+
+# compare Strings
+print("Apple" == 'Apple')
+ # We can compare the same string
+
+# the comparison must be between exact 
+print("Apple" == "apple")
+
+ 
+
